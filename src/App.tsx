13d2713f050438590eb93dbc70bad561c699f3b1
Alessandro Annini <alessandro.annini@gmail.com>
2024-11-29 13:05:08 +0100

    Refactor App.tsx to import UC33eController from correct location and update props

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
-import UC33eController from '@/components/UC33eController';
+import { UC33eController } from '@/components/UC33e';
+import { useController } from '@/hooks/useMidiController';
+import { UT33e } from '@/device-maps/UC33e';
 
 function App() {
+  const { state, status, connectedDevices } = useController({
+    controllers: UT33e,
+    // debounceTime: 16, // ~60fps
+    debounceTime: 1,
+  });
+
   return (
     <>
-      <div className="container mx-auto p-4">
-        <UC33eController />
+      {/* <div className="container mx-auto p-4"> */}
+      <div className="">
+        <UC33eController
+          state={state}
+          status={status}
+          connectedDevices={connectedDevices}
+        />
       </div>
     </>
   );
